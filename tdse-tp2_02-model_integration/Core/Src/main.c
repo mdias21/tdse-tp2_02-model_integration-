1b358505e75cf153b125aeb4bb7781512ae70b75
Marcos Dias <mdias@fi.uba.ar>
2025-10-03 21:13:12 -0300

    update commit

--- tdse-tp2_02-model_integration/Core/Src/main.c
+++ tdse-tp2_02-model_integration/Core/Src/main.c
@@ -228,11 +228,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(BTN_B_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : BTN_C_Pin BTN_D_Pin */
-  GPIO_InitStruct.Pin = BTN_C_Pin|BTN_D_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pin : BTN_C_Pin */
+   GPIO_InitStruct.Pin = BTN_C_Pin;
+   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+   GPIO_InitStruct.Pull = GPIO_PULLUP;
+   HAL_GPIO_Init(BTN_C_GPIO_Port, &GPIO_InitStruct);
+
+   /*Configure GPIO pin : BTN_D_Pin */
+    GPIO_InitStruct.Pin = BTN_D_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(BTN_D_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
